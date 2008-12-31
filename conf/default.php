fa06000c3a86672b4ab77741d89bfbc2b26edf6e
Robin Gareus <robin@gareus.org>
2008-12-31 19:58:03 +0100

    typos

--- conf/default.php
+++ conf/default.php
@@ -2,10 +2,10 @@
 /**
  * Options for the tagentry Plugin
  */
-$conf['tagsrc']       = 'All tags'; // 
-$conf['namespace']    = 'tag'; // scan here for tags
-$conf['table']        = false; // arrange tags in a table
-$conf['limit']        = 0; // 0: unlimited
-$conf['blacklist']    = ''; //
+$conf['tagsrc']       = 'All tags'; // where to get the tags from
+$conf['namespace']    = 'tag';      // scan here for tags
+$conf['table']        = false;      // arrange tags in a table
+$conf['limit']        = 0;          // 0: unlimited
+$conf['blacklist']    = '';         // space separated list
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
