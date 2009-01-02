756b9d81bd8f33efb74ea4669ecb177aaeadd739
Robin Gareus <robin@gareus.org>
2009-01-02 01:49:52 +0100

    added 'start' as default blacklist.

--- conf/default.php
+++ conf/default.php
@@ -6,6 +6,6 @@ $conf['tagsrc']       = 'All tags'; // where to get the tags from
 $conf['namespace']    = 'tag';      // scan here for tags
 $conf['table']        = false;      // arrange tags in a table
 $conf['limit']        = 0;          // 0: unlimited
-$conf['blacklist']    = '';         // space separated list
+$conf['blacklist']    = 'start';    // space separated list
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
