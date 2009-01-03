f46ae29cd90fb49d441d10fbaf848216021cfd92
Robin Gareus <robin@gareus.org>
2009-01-03 17:42:06 +0100

    removed 5 pre row limit if not using table layout

--- action.php
+++ action.php
@@ -238,7 +238,7 @@ class action_plugin_tagentry extends DokuWiki_Action_Plugin {
       if ($i%5==0 && $i!=0) { 
         if ($options['tagboxtable']) $rv.="</tr>\n<tr>";
         #else $rv.="<br/>\n";
-        else $rv.="  </div><div>\n";
+        #else $rv.="  </div><div>\n";
       }
       $i++;
       if ($options['tagboxtable']) $rv.='<td>';
