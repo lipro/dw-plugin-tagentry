47eb45b92e7080a3d171914bd97f8338c43569af
Stephan Linz <linz@li-pro.net>
2016-08-21 01:30:54 +0200

    add Russian translation as provided by comicslate
    Copy from http://comicslate.org/_media/wiki/dokuplug/tagentry.tgz
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ lang/ru/settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Russian language file for config of the
+ * tagentry plugin
+ *
+ * @author    Robin Gareus <robin@gareus.org
+ */
+$lang['tagsrc']        = 'Список видимых тегов';
+$lang['namespace']     = 'Папка для списка тегов';
+$lang['blacklist']     = 'Блэклист (через пробел)';
+$lang['table']         = 'Выстроить теги в xHTML-таблицу';
+$lang['limit']         = 'Ограничить число тегов, &lt;1: без лимита';
+$lang['height']        = 'Высота бокса со списком, <em>float(em|px|pt)</em><br/>&lt;0: по размеру списка, &gt;0 фиксирован, 0 или пусто: из CSS';
+$lang['tablerowcnt']   = 'Количество тегов на строку xHTML-таблицы (умолчание:5)';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
